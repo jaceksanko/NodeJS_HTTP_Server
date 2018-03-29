8b34ed6d09d14cefa010707d0d6c67e2a03a44ec
Jacek Sanko <jacek.sanko@gmail.com>
2018-03-29 21:18:33 +0200

    Change url request

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 
 var server = http.createServer(function (request, response) {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
-    if (request.method === 'GET' && request.url === '/index.html') {
+    if (request.method === 'GET' && request.url === '/') {
         fs.readFile(__dirname + '/index.html', 'utf-8', function (err, data) {
             response.write(data);
             response.end();
